5ee9b37491a7d7bef0873222493e30d6b93427f3
leilei <1792335270@qq.com>
2020-10-07 18:21:29 +0800

    新建了一个login.js

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+$(function () {
+    /* 去注册账号 */
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    });
+    /* 去登录 */
+    $('#link_login').on('click', function () {
+        $('.login-box').show();
+        $('.reg-box').hide();
+    });
+})
