81deb9bffa597673a9c1bf7e90538b32d02260fa
leilei <1792335270@qq.com>
2020-10-09 09:01:37 +0800

    修改了login.html

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,7 +3,7 @@ $(function () {
     $('#link_reg').on('click', function () {
         $('.login-box').hide();
         $('.reg-box').show();
-    });
+    })
     /* 去登录 */
     $('#link_login').on('click', function () {
         $('.login-box').show();
